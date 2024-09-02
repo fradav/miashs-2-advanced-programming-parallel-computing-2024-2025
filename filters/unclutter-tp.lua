61e05c5cbb6507a858c8406ea09d38d5722e770e
François-David Collin <Francois-David.Collin@umontpellier.fr>
2024-09-02 21:06:20 +0200

    wip unclutter-tp

--- filters/unclutter-tp.lua
+++ filters/unclutter-tp.lua
@@ -2,20 +2,22 @@ function Div(element)
   -- only execute this filter on files not ending with "-sol.qmd"
   local dir = string.match(quarto.doc.input_file, "(.-)([^\\/]-%.?([^%.\\/]*))$")
   dir = dir:gsub("\\", "/")
-  if quarto.doc.input_file:match("-sol%.qmd$") ~= nil or dir:match("Solutions/$") then
+  if quarto.doc.input_file:match("-sol%.qmd$") ~= nil or dir:match("Solutions/$") ~= nil then
     return nil
   end
-  if element.classes[1] == "cell" then
-    if element.attr.attributes["tags"] ~= nil then
-      tags = element.attr.attributes["tags"]
-      -- find if there is the "solution" tag
-      if tags:find("\"solution\"") then
-        quarto.log.debug("Found cell solution")
-        return {}
-       end
-      end
-  elseif element.classes[1] == "solution" then
-    quarto.log.debug("Found solution")
-    return {}
+  if element.classes ~= nil then
+    if element.classes[1] == "cell" then
+      if element.attr.attributes["tags"] ~= nil then
+        tags = element.attr.attributes["tags"]
+        -- find if there is the "solution" tag
+        if tags:find("\"solution\"") then
+          quarto.log.debug("Found cell solution")
+          return {}
+        end
+        end
+    elseif element.classes[1]:match("^solution") ~= nil then
+      quarto.log.debug("Found solution")
+      return {}
+    end
   end
 end
